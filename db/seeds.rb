ece6294b1d3316b6cf65b4a7017d109c91363ebb
marieauxille denis <marieauxille@gmail.com>
2019-05-20 14:40:49 +0200

    second ex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "destroying restaurants"
+
+Restaurant.destroy_all
+p "done"
+
+
+p "Beginning the seed"
+Restaurant.create(name: "Alicheur", address: "rue Saint Maur", category: "french")
+Restaurant.create(name: "Chez Tang", address: "rue de Belleville", category: "chinese")
+Restaurant.create(name: "Pho", address: "rue du Montparnasse", category: "japanese")
+Restaurant.create(name: "Paul", address: "rue odessa", category: "belgian")
+Restaurant.create(name: "PizzaMama", address: "rue Vavin", category: "italian")
+p "Ending the seed"
